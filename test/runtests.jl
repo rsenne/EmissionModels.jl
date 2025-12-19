9571ef00fcf259b45b0e70f1bdf68e32dc9c120e
Ryan Senne <50930199+rsenne@users.noreply.github.com>
2025-12-18 21:37:46 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,17 +6,21 @@ using JuliaFormatter
 using StatsAPI
 
 @testset "EmissionModels.jl" begin
+    @testset "Code linting" begin
+        if VERSION >= v"1.10" && isempty(VERSION.prerelease)
+            JET.test_package(EmissionModels; target_modules=(EmissionModels,))
+        else
+            @info "Skipping JET on Julia $VERSION (requires >=1.10 and a release build)"
+        end
+    end
     @testset "Zero-inflated models" begin
         include("zeroinflated/test_poisson.jl")
     end
+
     @testset "Multivariate models" begin
         include("multivariate/test_t.jl")
     end
-    @testset "Code linting" begin
-        if VERSION >= v"1.10"
-            JET.test_package(EmissionModels; target_modules=(EmissionModels,))
-        end
-    end
+
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(EmissionModels)
     end
